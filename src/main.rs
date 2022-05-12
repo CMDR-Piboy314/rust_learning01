f1ddca3044f80b64c34450d3f76225d097863877
CMDR-Piboy314 <92357316+CMDR-Piboy314@users.noreply.github.com>
2022-05-11 20:44:32 -0400

    No hecks + schtuff

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,37 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("--- Guess the number - Rust edition ---");
 
-    println!("Please enter your guess!");
+    let random_number = rand::thread_rng().gen_range(1..101);
 
-    let mut guess = String::new();
+    //println!("The random number is: {}", random_number);
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line!");
-    
-    println!("You guessed: {}", guess);
+    loop {
+        println!("Please enter your guess!");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&random_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
